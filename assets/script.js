1dc611b6902ca0f456742214b2c3494be4290b9d
whosshazo <ryandeshazo1@gmail.com>
2022-01-16 14:37:29 -0500

    layout of page complete

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ var inputValue = document.querySelector('#weatherSearch')
 
 
 button.addEventListener('click',function(){
-    fetch('https://api.openweathermap.org/data/2.5/onecall?q="+inputValue.value+"lat={lat}&lon={lon}&exclude={alerts,}&appid={f90d646020d714f2dced46f00a203dcb}')
+    fetch()
     .then(response => response.json())
     .then(data => console.log(data))
     
